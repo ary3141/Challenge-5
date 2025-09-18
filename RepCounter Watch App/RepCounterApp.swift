080dabdf09577b6de57d5a578159b913e217560d
Muhammad Dwiva Arya Erlangga <aryae432@gmail.com>
2025-09-18 09:40:56 +0800

    Initial Commit

---
+++ RepCounter Watch App/RepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepCounterApp.swift
+//  RepCounter Watch App
+//
+//  Created by Muhammad Dwiva Arya Erlangga on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct RepCounter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
